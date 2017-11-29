81be116f0ecc174ff254771111f981d86f5dc7c8
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-11-29 15:49:59 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+var array = [
+  ["Location #1", "Date #1", "Time #1"],
+  ["Location #2", "Date #2", "Time #2"]
+];
+
+
 function showButtonClick() {
   $('.box').toggleClass('box-visible');
+}
+
+window.onload = function() {
+  var table = document.createElement("TABLE");
+  var tableHeader1 = document.createELement("TH");
+  var textHeader1 = document.createTextnode("Location");
+  tableHeader1.appendChild(textHeader1);
+  table.appendChild(tableHeader1);
+  var tableHeader2 = document.createELement("TH");
+  var textHeader2 = document.createTextnode("Date");
+  tableHeader2.appendChild(textHeader2);
+  table.appendChild(tableHeader2);
+  var tableHeader3 = document.createELement("TH");
+  var textHeader3 = document.createTextnode("Time");
+  tableHeader3.appendChild(textHeader3);
+  table.appendChild(tableHeader3);
+  for (var i = 0; i < array.length; i++) {
+    var node = document.createElement("TR");
+    var row = array[i];
+    for (var j = 0; j < row.length; j++)
+    {
+      var rowNode = document.createElement("TD");
+      var textData = document.createTextnode(row[j]);
+      rowNode.appendChild(textData);
+      node.appendChild(rowNode);
+    }
+    table.appendChild(node);
+  }
 }
